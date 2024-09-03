3c415fa857d148d5bf3f09d54ca48c9d8b29ade3
DoctorLi <valhalla2048@gmail.com>
2024-09-03 15:47:09 +0800

    update

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,6 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
-    // plugins: [
-    //   ['inline-dotenv', {
-    //     path: '.env', // 指定 .env 文件的路径
-    //   }],
-    // ],
+    presets: ['babel-preset-expo']
   };
 };
--- env.d.ts
+++
@@ -1,6 +0,0 @@
-declare namespace NodeJS {
-    interface ProcessEnv {
-      AZURE_OPENAI_ENDPOINT: string;
-      AZURE_OPENAI_API_KEY: string;
-    }
-  }
